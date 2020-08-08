7b2eaf42632f215431c4a6c2dfd5bf1750ae5d79
John Fleming <johndeclanfleming@gmail.com>
2020-08-08 11:55:16 -0500

    Add project files.

---
+++ LegoMinifigures/Figures/StarWars.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LegoMinifigures.Figures
+{
+    class StarWars
+    {
+        public bool Head { get; set; }
+        public bool Torso { get; set; }
+        public int Arms { get; set; }
+        public int Legs { get; set; }
+        public bool WeaponEquipped { get; set; }
+        private bool _forceUser { get; set; } = false;
+
+
+        public string Name { get; set; }
+        public string Role { get; set; }
+        public string Catchphrase { get; set; }
+        public string Weapon { get; set; }
+        private int _missionsCompleted { get; set; }
+
+        public StarWars(string name, string role, int missionsCompleted)
+        {
+            Name = name;
+            Role = role;
+            _missionsCompleted = missionsCompleted;
+            Head = true;
+            Torso = true;
+            WeaponEquipped = false;
+
+            if (role == "jedi")
+            {
+                _forceUser = true;
+            }
+
+            Console.WriteLine($"{Name} unlocked.");
+        }
+
+        public void GetCharacterInfo()
+        {
+            Console.WriteLine($"{Name}'s Character Statistics:");
+            Console.WriteLine($"Role: {Role}");
+
+            Console.WriteLine($"Completed missions: {_missionsCompleted}");
+
+            var CharacterWeapon = WeaponEquipped ? $"Weapon of choice: {Weapon}." : $"Weapon equipped: None";
+            Console.WriteLine(CharacterWeapon);
+
+            Console.WriteLine($"Catchphrase: `{Catchphrase}.`");
+        }
+
+        public void CompleteMission(string missionName)
+        {
+            if (!WeaponEquipped) 
+            {
+                if (Arms > 0)
+                {
+                    Console.WriteLine($"{Name} ran into battle without a weapon equipped and lost an arm!");
+                    Arms--;
+                    Console.WriteLine($"{Name} limb count: {Arms} arm(s) & {Legs} leg(s)");
+                }
+                else if (Legs > 0)
+                {
+                    Console.WriteLine($"{Name} ran into battle without a weapon equipped and lost a leg!");
+                    Legs--;
+                    Console.WriteLine($"{Name} limb count: {Arms} arm(s) & {Legs} leg(s)");
+                }
+                else Console.WriteLine($"{Name} doesn't have a weapon or any arms or legs. They should probably sit this one out.");
+            }
+            else
+            {
+                Console.WriteLine($"{Name} yells '{Catchphrase}!' {missionName} mission completed.");
+                _missionsCompleted++;
+            }
+        }
+
+        // equip weapon
+        public void EquipWeapon(string weaponName)
+        {
+            Console.WriteLine($"{Name} equips the trusty {weaponName}.");
+            Weapon = weaponName;
+            WeaponEquipped = true;
+        }
+
+        // become jedi
+        public void BecomeAJedi()
+        {
+           if (_missionsCompleted >= 50)
+            {
+                Console.WriteLine($"{Name} leveled up! {Name} is now a Jedi.");
+                Role = "Jedi";
+                Weapon = "Lightsaber";
+                _forceUser = true;
+            }
+           else
+            {
+                Console.WriteLine($"{Name}'s training is not yet complete. Complete 50 missions to become a Jedi.");
+            }
+        }
+    }
+}
---
+++ LegoMinifigures/Program.cs
@@ -0,0 +1,41 @@
+﻿using LegoMinifigures.Figures;
+using System;
+
+namespace LegoMinifigures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Han = new StarWars("Han Solo", "Smuggler", 49)
+            {
+                Arms = 2,
+                Legs = 2,
+                Catchphrase = "Never tell me the odds"
+            };
+
+            Han.GetCharacterInfo();
+            Han.BecomeAJedi();
+            Han.EquipWeapon("blaster");
+            Han.CompleteMission("Destroy the Death Star");
+            Han.BecomeAJedi();
+            Han.GetCharacterInfo();
+
+            var Anakin = new StarWars("Anakin Skywalker", "Padawan", 5)
+            {
+                Arms = 2,
+                Legs = 2,
+                Catchphrase = "I hate sand"
+            };
+
+            Anakin.GetCharacterInfo();
+            Anakin.BecomeAJedi();
+            Anakin.CompleteMission("Defeat Count Dooku");
+            Anakin.CompleteMission("Defeat Obi Wan");
+            Anakin.CompleteMission("Defeat Obi Wan");
+            Anakin.CompleteMission("Defeat Obi Wan");
+            Anakin.CompleteMission("Defeat Obi Wan");
+
+        }
+    }
+}
